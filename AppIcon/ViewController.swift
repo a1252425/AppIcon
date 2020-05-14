3e175ef4924eb4857c56b50768427f640ca9347a
shuai.shao <shuai.shao@zerosportsai.com>
2020-05-14 11:58:58 +0800

    add project

---
+++ AppIcon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppIcon
+//
+//  Created by DDS on 2020/5/14.
+//  Copyright © 2020 zerosportsai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '13.0'
+inhibit_all_warnings!
+
+target 'AppIcon' do
+  use_frameworks!
+  pod 'SnapKit'
+  pod 'SVProgressHUD'
+end
